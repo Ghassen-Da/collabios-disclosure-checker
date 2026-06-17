a88adec267e6d681edd67214c053f2785d299106
Ghassen-Da <56545375+Ghassen-Da@users.noreply.github.com>
2026-06-17 18:18:46 +0100

    Add checkDisclosure function for marketing compliance

---
+++ index.js
@@ -0,0 +1,10 @@
+function checkDisclosure(country) {
+  return {
+    country,
+    message: "Check local influencer marketing disclosure requirements."
+  };
+}
+
+module.exports = {
+  checkDisclosure
+};
